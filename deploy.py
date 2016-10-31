3fb7d05d78da86acced4e4197bc4b52d57bba0c0
corwin <corwin.amber@gmail.com>
2016-10-31 12:31:12 +0200

    Updated archived papers and JACM accepted version; added research statement and award.

--- deploy.py
+++ deploy.py
@@ -3,6 +3,6 @@ import os
 deployed_files = ["*"] #os.listdir('w')
 
 for fn in deployed_files:
-    os.system('scp -r w/%(filename)s %(username)s@%(hostname)s:html'
+    os.system('scp -r w/%(filename)s %(username)s@%(hostname)s:public_html'
               % dict(filename=fn, 
-                     username='shachar', hostname='nova.cs.tau.ac.il'))
+                     username='shachari', hostname='login.csail.mit.edu'))
